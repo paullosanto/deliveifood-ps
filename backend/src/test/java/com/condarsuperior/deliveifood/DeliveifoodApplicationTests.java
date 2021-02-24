719e91b77b5d0c0e54c1d4e8bda448db1f1e259b
paulo santos <paulosantos08.araujo@gmail.com>
2021-02-23 22:00:41 -0300

    projeto create

---
+++ backend/src/test/java/com/condarsuperior/deliveifood/DeliveifoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.condarsuperior.deliveifood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveifoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
